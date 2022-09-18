e4faf1fa83f9f3e852d328399f9054d2c354f961
amin-mamandi <amin.mamandi@gmail.com>
2022-09-18 10:59:52 -0500

    Lab3 updated

--- Lab3/lab_files/finder.c
+++ Lab3/lab_files/finder.c
@@ -14,6 +14,10 @@
 #define SORT_EXEC  "/bin/sort"
 #define HEAD_EXEC  "/usr/bin/head"
 
+#define INPUT_END 1     // INPUT_END means where the pipe takes input
+#define OUTPUT_END 0 	// OUTPUT_END means where the pipe produces output
+                                                  
+
 int main(int argc, char *argv[])
 {
   int status;
@@ -21,59 +25,65 @@ int main(int argc, char *argv[])
 
   int p1[2], p2[2], p3[2];
 
+  // [STDIN -> terminal_input, STDOUT -> terminal_output, fd[OUTPUT_END] -> pipe_input, fd[INPUT_END] -> pipe_output]
   //Checking if input parameters DIR, STR and NUM_FILES are correct
   if (argc != 4) {
     printf("usage: finder DIR STR NUM_FILES\n");
     exit(0);
   }
 
-  //STEP 1
-	//Initialize pipes p1, p2, and p3
+  //STEP 1	
+  //Initialize pipes p1, p2, and p3
   pipe(p1);
   pipe(p2);
   pipe(p3);
+
   pid_1 = fork();
   if (pid_1 == 0) {
     /* First Child */
-
     //STEP 2
-		//In this first child process, we want to send everything that is printed on the standard output, to the next child process through pipe p1
-		//So, redirect standard output of this child process to p1's write end - written data will be automatically available at pipe p1's read end
-		//And, close all other pipe ends except the ones used to redirect the above OUTPUT (very important)
-    //p1's write end = p1[1] = left end of pipe
-    //p1's read end = p1[0] = read end of pipe
+    //In this first child process, we want to send everything that is printed on the standard output, to the next child process through pipe p1
+    //So, redirect standard output of this child process to p1's write end - written data will be automatically available at pipe p1's read end		
+    //And, close all other pipe ends except the ones used to redirect the above OUTPUT (very important)
 
-    dup2(p1[1],STDOUT_FILENO);
+
+    dup2(p1[1], STDOUT_FILENO); 
     close(p1[0]);
     close(p2[0]);
     close(p2[1]);
     close(p3[0]);
     close(p3[1]);
+
     //STEP 3
     //Prepare a command string representing the find command (follow example from the slide)
     //Invoke execl for bash and find (use BASH_EXEC and FIND_EXEC as paths)
-    execl(FIND_EXEC,FIND_EXEC,argv[1],"-name","*.[ch]",(char*)NULL);
+    
+    execl(FIND_EXEC,FIND_EXEC, argv[1], "-name", "*.[ch]", (char*) NULL);
+    
     exit(0);
   }
 
   pid_2 = fork();
   if (pid_2 == 0) {
     /* Second Child */
-
     //STEP 4
     //In this second child process, we want to receive everything that is available at pipe p1's read end, and use the received information as standard input for this child process
-		//In this second child process, we want to send everything that is printed on the standard output, to the next child process through pipe p2
-		//So, redirect standard output of this child process to p2's write end - written data will be automatically available at pipe p2's read end
-		//And, close all other pipe ends except the ones used to redirect the above two INPUT/OUTPUT (very important)
-    dup2(p1[0],STDIN_FILENO);
-    dup2(p2[1],STDOUT_FILENO);
-    close(p1[1]);
+    //In this second child process, we want to send everything that is printed on the standard output, to the next child process through pipe p2
+    //So, redirect standard output of this child process to p2's write end - written data will be automatically available at pipe p2's read end		
+    //And, close all other pipe ends except the ones used to redirect the above two INPUT/OUTPUT (very important)
+   
+    dup2(p1[0], STDIN_FILENO);
+    dup2(p2[1], STDOUT_FILENO);
+    //close(p1[0]);
     close(p2[0]);
+    //close(p2[1]);
     close(p3[0]);
     close(p3[1]);
+
     //STEP 5
     //Invoke execl for xargs and grep (use XARGS_EXEC and GREP_EXEC as paths)
-    execl(XARGS_EXEC,XARGS_EXEC,GREP_EXEC,"-c",argv[2],(char*)NULL);
+    execl(XARGS_EXEC, XARGS_EXEC, GREP_EXEC, "-c", argv[2], (char*)NULL);
+    
     exit(0);
   }
 
@@ -81,43 +91,52 @@ int main(int argc, char *argv[])
   if (pid_3 == 0) {
     /* Third Child */
 
-    //STEP 6
-		//In this third child process, we want to receive everything that is available at pipe p2's read end, and use the received information as standard input for this child process
-		//In this third child process, we want to send everything that is printed on the standard output, to the next child process through pipe p3
-		//So, redirect standard output of this child process to p3's write end - written data will be automatically available at pipe p3's read end
-		//And, close all other pipe ends except the ones used to redirect the above two INPUT/OUTPUT (very important)
-    dup2(p2[0],STDIN_FILENO);
-    dup2(p3[1],STDOUT_FILENO);
+    //STEP 6		
+    //In this third child process, we want to receive everything that is available at pipe p2's read end, and use the received information as standard input for this child process		
+    //In this third child process, we want to send everything that is printed on the standard output, to the next child process through pipe p3
+    //So, redirect standard output of this child process to p3's write end - written data will be automatically available at pipe p3's read end	
+    //And, close all other pipe ends except the ones used to redirect the above two INPUT/OUTPUT (very important)
+    
+    dup2(p2[0], STDIN_FILENO);
+    dup2(p3[1], STDOUT_FILENO);
+
+    //close(p2[0]);
+    close(p2[1]);
+    close(p3[0]);
+    //close(p3[1]);
     close(p1[1]);
     close(p1[0]);
-    close(p2[1]);
-    close(p3[1]);
 
     //STEP 7
     //Invoke execl for sort (use SORT_EXEC as path)
+    
     execl(SORT_EXEC,SORT_EXEC,"-t", ":","+1.0","-2.0","--numeric","--reverse",(char*)NULL);
+    
     exit(0);
   }
 
   pid_4 = fork();
   if (pid_4 == 0) {
     /* Fourth Child */
-
     //STEP 8
-		//In this fourth child process, we want to receive everything that is available at pipe p3's read end, and use the received information as standard input for this child process
-		//Output of this child process should directly be to the standard output and NOT to any pipe
-		//And, close all other pipe ends except the ones used to redirect the above INPUT (very important)
-    dup2(p3[0],STDIN_FILENO);
+    //In this fourth child process, we want to receive everything that is available at pipe p3's read end, and use the received information as standard input for this child process
+    //Output of this child process should directly be to the standard output and NOT to any pipe
+    //And, close all other pipe ends except the ones used to redirect the above INPUT (very important)
+    
+    dup2(p3[0], STDIN_FILENO);
+    close(p3[1]);
+    //close(p3[0]);
     close(p1[0]);
     close(p1[1]);
     close(p2[0]);
     close(p2[1]);
-    close(p3[1]);
 
     //STEP 8
     //Invoke execl for head (use HEAD_EXEC as path)
     //char* lines[2]= {"--lines=",(char*)argv[3]};
-    execl(HEAD_EXEC,HEAD_EXEC,"--lines=5",(char*)NULL);
+   
+    execl(HEAD_EXEC,HEAD_EXEC,"--lines=5", (char*)NULL);
+   
     exit(0);
   }
 
