25790c492128079a50bbdca00a4a777f042c2c34
Amin Mamandipoor <a972m888@starfight.ittc.ku.edu>
2022-10-14 09:27:08 -0500

    in case

--- Project_1/Quash.c
+++ Project_1/Quash.c
@@ -30,6 +30,8 @@ int main()
 	fflush(stdout);
 	int pid;
 	char cmd[30];
+	char * token;
+	const char s[2] = " ";
 
 	//signals
 	struct sigaction sa_init;
@@ -40,48 +42,55 @@ int main()
 
 	while(1) {
 		printf("\n");
-		char cmd[30];
+		fflush(stdout);
 		fgets(cmd, sizeof(cmd), stdin);
-		cmd[strcspn(cmd, "\n")] = 0; 
-		
-		if (  strcmp(cmd, "ls") ==  0){ 
-			pid = fork();
-			if (pid == 0){
-				printf("\n");
-				execl("/bin/ls", "ls", NULL);
-			} else{
-				wait(NULL);
-			}
-		}
-		else if ( strcmp(cmd, "ls -a") == 0){
-                        pid = fork();
-                        if (pid == 0){
-                                printf("\n");
-                                execl("/bin/ls", "ls", "-a", NULL);
-                        } else{
-                                wait(NULL);
-                        }
-		}	
-		else if( strcmp(cmd, "pwd") == 0){
-			pid = fork();
-			if(pid == 0){
-				printf("\n");
-				execl("/bin/pwd", "pwd", NULL);
-			}
-			else {
-				wait(NULL);
-			}
-		}
-              else if( strcmp(cmd, "echo") == 0){
-                        pid = fork();
-                        if(pid == 0){
-                                execl("/bin/echo", "echo",  NULL);
-                        }
-                        else {
-                                wait(NULL);
-                        }
-                }
+		cmd[strcspn(cmd, "\n")] = 0;
+		token = strtok(cmd, s);
 
+			if (strcmp(token, "ls") == 0){
+				token = strtok(NULL, s);
+				//printf(" %s\n", token);
+				pid = fork();
+				if (pid == 0){
+					printf("\n %s", token);
+					if (strcmp(token, "-a") != 0)
+						execl("/bin/ls", "ls", NULL);
+					else
+						execl("/bin/ls", "ls", "-a", NULL);
+				} else{
+					wait(NULL);
+				}
+			}
+			/*else if ( strcmp(token, "ls -a") == 0){
+                        	pid = fork();
+                        	if (pid == 0){
+                               		printf("\n");
+                                	execl("/bin/ls", "ls", "-a", NULL);
+                        	} else{
+                                	wait(NULL);
+                        	}
+			}*/	
+			else if( strcmp(token, "pwd") == 0){
+				pid = fork();
+				if(pid == 0){
+					printf("\n");
+					execl("/bin/pwd", "pwd", NULL);
+				}
+				else {
+					wait(NULL);
+				}
+			}
+              		else if( strcmp(token, "echo") == 0){
+                        		pid = fork();
+                        		if(pid == 0){
+						token = strtok(NULL, s);
+						execl("/bin/echo", "echo", strtok(strtok(NULL, '"'), '"'), NULL);
+					}
+                        		else {
+                                		wait(NULL);
+					}
+			}
 	}
+	
 	return 0;
 }
