7940255855db5fb9ff2b1bfab745a87ae0de3e8f
amin-mamandi <amin.mamandi@ku.edu>
2022-11-23 13:18:46 -0600

    lab file 9-10 added

---
+++ 10-Paging/lab_files/VM_addr_map.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define MAXSTR 1000
+
+int main(int argc, char *argv[])
+{
+  char line[MAXSTR];
+  int *page_table, *mem_map;
+  unsigned int log_size, phy_size, page_size, d;
+  unsigned int num_pages, num_frames;
+  unsigned int offset, logical_addr, physical_addr, page_num, frame_num;
+
+  /* Get the memory characteristics from the input file */
+  fgets(line, MAXSTR, stdin);
+  if((sscanf(line, "Logical address space size: %d^%d", &d, &log_size)) != 2){
+    fprintf(stderr, "Unexpected line 1. Abort.\n");
+    exit(-1);
+  }
+  fgets(line, MAXSTR, stdin);
+  if((sscanf(line, "Physical address space size: %d^%d", &d, &phy_size)) != 2){
+    fprintf(stderr, "Unexpected line 2. Abort.\n");
+    exit(-1);
+  }
+  fgets(line, MAXSTR, stdin);
+  if((sscanf(line, "Page size: %d^%d", &d, &page_size)) != 2){
+    fprintf(stderr, "Unexpected line 3. Abort.\n");
+    exit(-1);
+  }
+
+  /* Allocate arrays to hold the page table and memory frames map */
+  
+
+  /* Initialize page table to indicate that no pages are currently mapped to
+     physical memory */
+  
+
+  /* Initialize memory map table to indicate no valid frames */
+  
+
+  /* Read each accessed address from input file. Map the logical address to
+     corresponding physical address */
+  fgets(line, MAXSTR, stdin);
+  while(!(feof(stdin))){
+    sscanf(line, "0x%x", &logical_addr);
+    fprintf(stdout, "Logical Address: 0x%x\n", logical_addr);
+    
+	/* Calculate page number and offset from the logical address */
+    
+
+    /* Form corresponding physical address */
+    
+
+    /* Read next line */
+    fgets(line, MAXSTR, stdin);    
+  }
+
+  return 0;
+}
